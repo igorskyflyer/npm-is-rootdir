4a741fb6f5131608ec3eb3c032626c6639ddd293
igorskyflyer <igor.dvlpr@gmail.com>
2025-08-31 14:56:24 +0200

    Migrate to .ts

---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import { slash, u, uw, ux } from '@igorskyflyer/upath'
+import { platform } from 'node:os'
+
+const os=platform()
+const isWindows=os==='win32'
+
+const winDirectoryCount=2
+const unixDirectoryCount=1
+const rootDirectoryCount=isWindows? winDirectoryCount:unixDirectoryCount
+
+/**
+ * Checks whether the given path is a root folder/drive path.
+ * @private
+ * @param {string} path
+ * @param {string} osSlash
+ * @param {number} count
+ * @returns {boolean}
+ */
+function checkRootDirectory(path, osSlash, count) {
+  if (!path) {
+    return false
+  }
+
+  return (path.split(osSlash).filter((value) => value.length>0).length<count)
+}
+
+/**
+ * Checks whether the given path is a root folder/drive path for host OS.
+ * @param {string} path
+ * @returns {boolean}
+ */
+export function isRootDirectory(path) {
+  return checkRootDirectory(u(path, true), slash, rootDirectoryCount)
+}
+
+/**
+ * Checks whether the given path is a root folder/drive path for Windows OS.
+ * @param {string} path
+ * @returns {boolean}
+ */
+export function isRootDirectoryWin(path) {
+  return checkRootDirectory(uw(path, true), '\\', winDirectoryCount)
+}
+
+/**
+ * Checks whether the given path is a root folder/drive path for UNIX-like OS'.
+ * @param {string} path
+ * @returns {boolean}
+ */
+export function isRootDirectoryUnix(path) {
+  return checkRootDirectory(ux(path, true), '/', unixDirectoryCount)
+}
